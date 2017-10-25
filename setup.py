ddd069ef0d2ee377747e25bf14674ec32c0a8baf
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2017-10-25 13:59:24 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+setup(
+    name='tdx',
+    version='0.01',
+    description='A Python wrapper of pytdx',
+    long_description=long_description,
+    author='Jie Wang',
+    author_email='790930856@qq.com',
+    url='https://github.com/JaysonAlbert/tdx',
+    packages=find_packages(),
+    install_requires=[
+        'pytdx',
+        'pandas'
+    ]
+)
